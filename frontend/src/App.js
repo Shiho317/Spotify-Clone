22a34d914cede9dc6e6e4d4b379fa3cf8c8f547f
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-04-05 11:03:35 -0700

    added backend environment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,29 @@
+import { useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  console.log()
+
+  const getPlaylist = () => {
+    axios.get(`https://api.spotify.com/v1/users/${process.env.REACT_APP_USER_ID}/playlists/${process.env.REACT_APP_PLAYLIST_ID}`, {
+      headers: {
+              "Authorization": `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`,
+            },
+    })
+    .then((res) => {
+      console.log(res.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
+  useEffect(() => {
+    getPlaylist();
+  },[])
+
   return (
     <div>
     Hello
