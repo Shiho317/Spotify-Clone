d0fc21f84b177e9d7273fe33c2bacb29fb7439d6
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-04-05 19:39:17 -0700

    added styles

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import { HeaderWrapper } from './Header.style'
+import React from 'react';
+import { HeaderWrapper, LogoWrapper, NavigationsWrapper } from './Header.style';
+import SpotifyLogo from '../../assets/images/Spotify_Logo_RGB_White.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <HeaderWrapper>
-      Header
+        <LogoWrapper>
+          <img src={SpotifyLogo} alt='logo'/>
+        </LogoWrapper>
+        <div>
+          <NavigationsWrapper>
+            <ul>
+              <li>
+                <Link to='/'>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to='/'>
+                  Search
+                </Link>
+              </li>
+              <li>
+                <Link to='/'>
+                  My library
+                </Link>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                Create Playlists
+              </li>
+              <li>
+                Your favorite
+              </li>
+            </ul>
+          </NavigationsWrapper>
+          <div>
+            <ul>
+              <li></li>
+            </ul>
+          </div>
+        </div>
     </HeaderWrapper>
   )
 }
--- frontend/src/Components/Header/Header.style.js
+++ frontend/src/Components/Header/Header.style.js
@@ -1,4 +1,29 @@
 import styled from "styled-components";
 
 export const HeaderWrapper = styled.div`
+  background: var(--black-color);
+  width: 17%;
+  height: 100vh;
+  padding: 1.5rem;
+`
+
+export const LogoWrapper = styled.div`
+  width: 100%;
+
+  & img{
+    width: 70%;
+  }
+`
+
+export const NavigationsWrapper = styled.nav`
+  background: pink;
+
+  & ul{
+    display: flex;
+    flex-direction: column;
+  }
+
+  & li{
+    list-style: none;
+  }
 `
